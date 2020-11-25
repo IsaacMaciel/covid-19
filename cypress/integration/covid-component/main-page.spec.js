da871c1d5cd8719bc56a0aa0561894675ddb6062
Isaac Maciel de Araújo Neto <Isaac.neto@mcmmontagens.com.br>
2020-11-24 21:24:30 -0300

    refatorando os testes cypress

--- cypress/integration/covid-component/main-page.spec.js
+++ cypress/integration/covid-component/main-page.spec.js
@@ -1,50 +1,64 @@
+const cardCountry = {
+  TITLE_CARD: '.sc-gKsewC.hyuxfC',
+  CARD_BODY: '.sc-fubCfw.ddZyYg',
+};
+const cardUf = {
+  TITLE_CARD: '.sc-iqHYGH.chWaMa',
+  CARD_BODY: '.sc-dQppl.hFqyJy',
+};
+const LIST_CONTAINER = '.sc-bqyKva.hFbVhV';
+const SEARCH = '.sc-bkzZxe.jzVNJi';
+
+// const TITLE_CARD = '.sc-gKsewC.hyuxfC';
 describe('Covid Ui', () => {
   it('Should return the application title', () => {
     cy.visit('/');
     cy.contains('Covid-19 Brasil').should('to.have.length', 1);
   });
   it('Should display the information Country Brazil', () => {
-    cy.get('.sc-gKsewC.hyuxfC').contains('Brasil');
-    cy.get('.sc-fubCfw.ddZyYg')
+    cy.get(cardCountry.TITLE_CARD).contains('Brasil');
+    cy.get(cardCountry.CARD_BODY)
       .contains('Confirmados')
       .should('to.have.length', 1);
 
-    cy.get('.sc-fubCfw.ddZyYg').contains('Ativos').should('to.have.length', 1);
+    cy.get(cardCountry.CARD_BODY)
+      .contains('Ativos')
+      .should('to.have.length', 1);
 
-    cy.get('.sc-fubCfw.ddZyYg')
+    cy.get(cardCountry.CARD_BODY)
       .contains('Recuperados')
       .should('to.have.length', 1);
 
-    cy.get('.sc-fubCfw.ddZyYg').contains('Mortos').should('to.have.length', 1);
+    cy.get(cardCountry.CARD_BODY)
+      .contains('Mortos')
+      .should('to.have.length', 1);
 
-    cy.get('.sc-fubCfw.ddZyYg')
+    cy.get(cardCountry.CARD_BODY)
       .contains('Atualizado em')
       .should('to.have.length', 1);
   });
   it('Should have 27 state + 1 country in List', () => {
     cy.visit('/');
-    cy.get('.sc-bqyKva.hFbVhV').should('to.have.length', 28);
+    cy.get(LIST_CONTAINER).should('to.have.length', 28);
   });
   it('Should search UF', () => {
     cy.visit('/');
 
-    cy.get('.sc-bkzZxe.jzVNJi').type('Ama');
-    cy.get('.sc-bqyKva.hFbVhV').should('to.have.length', 2);
-    cy.get('.sc-bqyKva.hFbVhV').contains('Amapá');
-    cy.get('.sc-bqyKva.hFbVhV').contains('Amazonas');
+    cy.get(SEARCH).type('Ama');
+    cy.get(LIST_CONTAINER).should('to.have.length', 2);
+    cy.get(LIST_CONTAINER).contains('Amapá');
+    cy.get(LIST_CONTAINER).contains('Amazonas');
   });
 
   it('Should display information UF', () => {
     cy.visit('/');
-    cy.get('.sc-bqyKva.hFbVhV').contains('Pernambuco').click();
-    cy.get('.sc-iqHYGH.chWaMa').contains('Pernambuco');
+    cy.get(LIST_CONTAINER).contains('Pernambuco').click();
+    cy.get(cardUf.TITLE_CARD).contains('Pernambuco');
 
-    cy.get('.sc-dQppl.hFqyJy').contains('Casos').should('to.have.length', 1);
-    cy.get('.sc-dQppl.hFqyJy')
-      .contains('Suspeitos')
-      .should('to.have.length', 1);
-    cy.get('.sc-dQppl.hFqyJy').contains('Mortos').should('to.have.length', 1);
-    cy.get('.sc-dQppl.hFqyJy')
+    cy.get(cardUf.CARD_BODY).contains('Casos').should('to.have.length', 1);
+    cy.get(cardUf.CARD_BODY).contains('Suspeitos').should('to.have.length', 1);
+    cy.get(cardUf.CARD_BODY).contains('Mortos').should('to.have.length', 1);
+    cy.get(cardUf.CARD_BODY)
       .contains('Atualizado em')
       .should('to.have.length', 1);
   });
